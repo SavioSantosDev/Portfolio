72fcdccd9c262dde1435f4b961de7c37271f12b8
saviogta1 <saviogta1@gmail.com>
2021-01-03 19:39:30 -0300

    formulario de contato

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
 import { IFormCanDeativate } from 'src/models/IFormCanDeactivate';
@@ -25,7 +26,8 @@ export class ContactComponent implements OnInit, OnDestroy, IFormCanDeativate {
   sub: Subscription;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private http: HttpClient
   ) { }
 
 
@@ -81,7 +83,42 @@ export class ContactComponent implements OnInit, OnDestroy, IFormCanDeativate {
 
 
   onSubmit(): void {
-    console.log('formulario submetido!');
+    if (this.formulario.status === 'VALID') {
+
+      this.formulario.disable();  // Disabilitar o formulário para não acontecer multiplas subimissões
+
+      // Criando o formData e adicionando os valores
+      const formData: any = new FormData();
+      formData.append('nome', this.nome.value);
+      formData.append('email', this.email.value);
+      formData.append('phone', this.phone.value);
+      formData.append('assunto', this.assunto.value);
+      formData.append('mensagem', this.mensagem.value);
+
+      console.log(this.formulario.value);
+
+      const httpBin = 'https://httpbin.org/post';
+      const myScriptContact = 'https://script.google.com/macros/s/AKfycbyGJjZWx_o-IQseao5UH06jTsTN7gQW5yqeq_5XmA/exec';
+
+      this.http.post(myScriptContact, formData).subscribe(
+        (response) => {
+          // choose the response message
+          if (response["result"] === "success") {
+            this.formulario.reset();
+            alert('Mensagem enviada com sucesso!');
+          } else {
+            alert('Ops! Ocorreu algum erro, recarregue a página e tente novamente.');
+          }
+          this.formulario.enable();   // Deixar o formulário editável novamente
+          console.log(response);
+        },
+        (error) => {
+          alert('Ops! Ocorreu algum erro, recarregue a página e tente novamente.');
+          this.formulario.enable(); // re enable the form after a success
+          console.log(error);
+        }
+      );
+    }
   }
 
 
